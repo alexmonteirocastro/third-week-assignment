88f76cbace6b912218c76b7e7d1439a742775318
alexmonteirocastro <alex.monteiro@outlook.pt>
2019-04-08 00:14:13 +0200

    First endpoint and nodemon as dev dependency

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const Koa = require('koa')
 const Router = require('koa-router')
 
+const app = new Koa()
+const router = new Router()
+const PORT = 3000
+
+router.get('/', async (ctx, next) => {
+    ctx.body = 'AHoj'
+    next()
+})
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods())
+    .listen(PORT, () => console.log('Up and running on port ' + PORT))
