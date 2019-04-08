5c784f9950724067d9242b394209e721a176b943
alexmonteirocastro <alex.monteiro@outlook.pt>
2019-04-08 23:24:53 +0200

    Add eslint to project

--- articles.js
+++ articles.js
@@ -1,12 +1,12 @@
-'use strict'
+'use strict';
 
-const articles = new Map()
+const articles = new Map();
 
-articles.set(0, 'This is an article.')
-articles.set(1, 'This is another article.')
-articles.set(2, 'This is yet another article.')
-articles.set(3, 'This article is about Map data structures in JavaScript.')
-articles.set(4, 'This article is about KOA API framework.')
-articles.set(5, 'This article is about RESTful API.')
+articles.set(0, 'This is an article.');
+articles.set(1, 'This is another article.');
+articles.set(2, 'This is yet another article.');
+articles.set(3, 'This article is about Map data structures in JavaScript.');
+articles.set(4, 'This article is about KOA API framework.');
+articles.set(5, 'This article is about RESTful API.');
 
-module.exports = articles
+module.exports = articles;
